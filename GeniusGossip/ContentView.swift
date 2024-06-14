a495bc24e116778f915e26406c0544fbac63bad4
Nikhil Barik <nklmarch17@gmail.com>
2024-06-14 06:27:34 +0530

    feat: Implement basic UI with text field for user input 🎉

--- GeniusGossip/ContentView.swift
+++ GeniusGossip/ContentView.swift
@@ -10,12 +10,33 @@ import GoogleGenerativeAI
 
 struct ContentView: View {
     let model = GenerativeModel(name: "gemini-pro", apiKey: APIKey.default)
+    
+    @State var userPrompt = ""
+    @State var response = "What adventure are we embarking on today?"
+    @State var isLoading = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hey there!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.top, 50)
+            ZStack{
+                ScrollView{
+                    Text(response)
+                }
+                
+                if isLoading{
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                        .scaleEffect(4)
+                }
+            }
+            TextField("Message GeniusGossip",text: $userPrompt, axis: .vertical)
+                .lineLimit(5)
+                .font(.title3)
+                .padding()
+                .background(Color(.systemGray6), in: Capsule())
         }
         .padding()
     }
