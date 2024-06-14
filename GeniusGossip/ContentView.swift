5b3166cf91864d29f3de932f663aa9ee7dfbb68a
Nikhil Barik <nklmarch17@gmail.com>
2024-06-14 05:50:31 +0530

    Initial Commit

---
+++ GeniusGossip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeniusGossip
+//
+//  Created by Nikhil Barik on 14/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeniusGossip/GeniusGossipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeniusGossipApp.swift
+//  GeniusGossip
+//
+//  Created by Nikhil Barik on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GeniusGossipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
